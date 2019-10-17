e3b70d32fc133ec78fef5dc8b6926481af80518c
Kellis <34947108+Gumbraise@users.noreply.github.com>
2019-10-18 00:41:16 +0200

    v0.1.1810

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Nachos
-A Snap Score Giver
+### Snapchat Score Giver
+
+## Use
+
+Put how much money do you want to spend for your score and our algorithm will send you how many Snapchat score de you could have.
+The price is exponential. The mathematics function is ``((1.009637726^X)-1)*10000``
+
+## Payment
+
+The payment is done by <i>Bitcoin</i> or <i>Ethereum</i> for now. <i>PayPal</i> will be available in next versions. 
+
+## Warning
+
+If you are under 16 years old <strong>don't use this program</strong> without a major person.
+
+## Donate
+
+If you want to help me in my work, consider donating. Thank you!
+
+### Bitcoin
+
+- 3ML4B58aw9CHTsjE5nPGzVhTLK8mBz8Vv4
+
+### Ethereum
+
+- 0xf1fee16e2b91b462139434b9c5FE949856ccc6eb
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,39 @@
 var money = document.getElementById("money");
 var points = document.getElementById("points");
-//var button = document.getElementById("submit");
+var button = document.getElementById("pay");
+var div_pay = document.getElementById("div_pay");
+var ppp = document.getElementById("ppp");
 
-var mtp = document.getElementById("mtp");
-var ptm = document.getElementById("ptm");
-
-/*button.onclick = */function moneytopoints() { 
+function moneytopoints() { 
+    var val_money = money.value;
     if (val_money <= 250) {
-        var val_money = money.value;
-        var result_points = Math.round(((Math.pow(1.009637726, val_money) - 1) * (Math.pow(10, 5))));
-        points.value = result_points;
-        setInterval(100);
-    }
-    else if (val_money > 0) {
-        money.value = 0;
-        setInterval(100);
+        if (val_money >= 0) {
+            var val_money = money.value;
+            var result_points = Math.round(((Math.pow(1.009637726, val_money) - 1) * (Math.pow(10, 5))));
+            points.value = result_points;
+            setInterval(100);
+        }
+        else {
+            var val_money = money.value;
+
+            money.value = 0;
+            setInterval(100);
+        }
     }
     else if (val_money > 250) {
+        var val_money = money.value;
+
         money.value = 250;
         setInterval(100);
     }
 }
 
-/*
-function pointstomoney() {
-    
-        var val_points = points.value;
-            
-        var result_money = Math.round(((Math.pow(val_points, 1/1.09637726) - 1) * (Math.pow(10, -4))));
-
-        money.value = result_money;
+button.onclick = function() {
+    div_pay.style.display = "block";
+    ppp.innerHTML = "Waiting to receive " + money.value + "€ in Bitcoin or Ethereum at these adresses:";
 }
-*/
+window.onclick = function(event) {
+    if (event.target == div_pay) {
+      div_pay.style.display = "none";
+    }
+  }
